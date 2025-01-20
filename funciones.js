8ac5521dc101cc9d8ce6611f19593310451cdb77
AlejandroV <alejandro.vasconez.mosquera@ces-vegamedia.es>
2025-01-20 11:37:09 +0100

    Primera función
    Usamos IF, bàsicamente

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var resultado=confirm("¿Quieres hacer una operación aritmética?");
+console.log("Tu resultado --> "+resultado);
+if(resultado){
+    alert("Si :D");
+}
+else{
+    alert("No >:C");
+}
